ad306e2570c058fd4804aa49e94d93db89ee4f52
Ozan Bayram <ozan_bayram@epam.com>
2022-11-28 15:51:42 +0300

    Added ParallelEfficiencyTest for 4x4 matrices.

--- MultiThreading.Task3.MatrixMultiplier.Tests/MultiplierTest.cs
+++ MultiThreading.Task3.MatrixMultiplier.Tests/MultiplierTest.cs
@@ -25,6 +25,19 @@ namespace MultiThreading.Task3.MatrixMultiplier.Tests
         {
             // todo: implement a test method to check the size of the matrix which makes parallel multiplication more effective than
             // todo: the regular one
+
+            var timer1 = System.Diagnostics.Stopwatch.StartNew();
+            TestMatrix4On4(new MatricesMultiplier());
+            timer1.Stop();
+
+            var timer2 = System.Diagnostics.Stopwatch.StartNew();
+            TestMatrix4On4(new MatricesMultiplierParallel());
+            timer2.Stop();
+
+            var duration1 = timer2.ElapsedMilliseconds;
+            var duration2 = timer2.ElapsedMilliseconds;
+
+            Assert.IsTrue(duration1 < duration2, "Parallel one is not efficient for 4x4 matrices.");
         }
 
         #region private methods
@@ -76,6 +89,57 @@ namespace MultiThreading.Task3.MatrixMultiplier.Tests
             Assert.AreEqual(728, multiplied.GetElement(2, 2));
         }
 
+        void TestMatrix4On4(IMatricesMultiplier matrixMultiplier)
+        {
+            if (matrixMultiplier == null)
+            {
+                throw new ArgumentNullException(nameof(matrixMultiplier));
+            }
+
+            var m1 = new Matrix(4, 4);
+            m1.SetElement(0, 0, 34);
+            m1.SetElement(0, 1, 2);
+            m1.SetElement(0, 2, 6);
+            m1.SetElement(0, 3, 1);
+
+            m1.SetElement(1, 0, 5);
+            m1.SetElement(1, 1, 4);
+            m1.SetElement(1, 2, 54);
+            m1.SetElement(1, 3, 2);
+
+            m1.SetElement(2, 0, 2);
+            m1.SetElement(2, 1, 9);
+            m1.SetElement(2, 2, 8);
+            m1.SetElement(2, 3, 1);
+
+            m1.SetElement(3, 0, 1);
+            m1.SetElement(3, 1, 6);
+            m1.SetElement(3, 2, 1);
+            m1.SetElement(3, 3, 4);
+
+            var m2 = new Matrix(4, 4);
+            m2.SetElement(0, 0, 12);
+            m2.SetElement(0, 1, 52);
+            m2.SetElement(0, 2, 85);
+            m2.SetElement(0, 3, 44);
+
+            m2.SetElement(1, 0, 5);
+            m2.SetElement(1, 1, 5);
+            m2.SetElement(1, 2, 54);
+            m2.SetElement(1, 3, 11);
+
+            m2.SetElement(2, 0, 5);
+            m2.SetElement(2, 1, 8);
+            m2.SetElement(2, 2, 9);
+            m2.SetElement(2, 3, 4);
+
+            m2.SetElement(3, 0, 2);
+            m2.SetElement(3, 1, 6);
+            m2.SetElement(3, 2, 1);
+            m2.SetElement(3, 3, 7);
+
+            var multiplied = matrixMultiplier.Multiply(m1, m2);
+        }
         #endregion
     }
 }
